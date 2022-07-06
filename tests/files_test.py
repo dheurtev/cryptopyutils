e47788b8b8065d77813d8cfadfccf9c9813259df
david HEURTEVENT <david@heurtevent.org>
2022-07-06 14:12:54 +0200

    Syntax fixes

--- tests/files_test.py
+++ tests/files_test.py
@@ -9,59 +9,59 @@ from cryptopyutils import files
 
 def test_filepath():
     """Test filepath generation : Standard case"""
-    dir = "/tmp"
+    dir_path = "/tmp"
     host = "www.example.com"
     ext = "pem"
-    fp = files.generate(host, dir, ext)
-    assert str(fp) == "/tmp/www.example.com.pem"
+    file_path = files.generate(host, dir_path, ext)
+    assert str(file_path) == "/tmp/www.example.com.pem"
 
 
 def test_key():
     """Test filepath generation : key file"""
-    dir = "/tmp"
+    dir_path = "/tmp"
     host = "www.example.com"
-    fp = files.key(host, dir)
-    assert str(fp) == "/tmp/private/www.example.com.key"
+    file_path = files.key(host, dir_path)
+    assert str(file_path) == "/tmp/private/www.example.com.key"
 
 
 def test_crt():
     """Test filepath generation : crt file"""
-    dir = "/tmp"
+    dir_path = "/tmp"
     host = "www.example.com"
-    fp = files.crt(host, dir)
-    assert str(fp) == "/tmp/certs/www.example.com.crt"
+    file_path = files.crt(host, dir_path)
+    assert str(file_path) == "/tmp/certs/www.example.com.crt"
 
 
 def test_csr():
     """Test filepath generation : csr file"""
-    dir = "/tmp"
+    dir_path = "/tmp"
     host = "www.example.com"
-    fp = files.csr(host, dir)
-    assert str(fp) == "/tmp/csr/www.example.com.csr"
+    file_path = files.csr(host, dir_path)
+    assert str(file_path) == "/tmp/csr/www.example.com.csr"
 
 
 def test_pem():
     """Test filepath generation : pem file"""
-    dir = "/tmp"
+    dir_path = "/tmp"
     host = "www.example.com"
-    fp = files.pem(host, dir)
-    assert str(fp) == "/tmp/private/www.example.com.pem"
+    file_path = files.pem(host, dir_path)
+    assert str(file_path) == "/tmp/private/www.example.com.pem"
 
 
 def test_der():
     """Test filepath generation : der file"""
-    dir = "/tmp"
+    dir_path = "/tmp"
     host = "www.example.com"
-    fp = files.der(host, dir)
-    assert str(fp) == "/tmp/private/www.example.com.der"
+    file_path = files.der(host, dir_path)
+    assert str(file_path) == "/tmp/private/www.example.com.der"
 
 
 def test_pub():
     """Test filepath generation : pub file"""
-    dir = "/temp"
+    dir_path = "/temp"
     host = "www.example.com"
-    fp = files.pub(host, out_dir=dir)
-    assert str(fp) == "/temp/www.example.com.pub"
+    file_path = files.pub(host, out_dir=dir_path)
+    assert str(file_path) == "/temp/www.example.com.pub"
 
 
 def test_file_exists():
@@ -69,11 +69,11 @@ def test_file_exists():
     filepath = "/tmp/tmpfile12395698744"
     if os.path.exists(filepath):
         os.remove(filepath)
-    assert not (files.file_exists(filepath))
+    assert not files.file_exists(filepath)
     Path(filepath).touch()
     assert files.file_exists(filepath)
     os.remove(filepath)
-    assert not (files.file_exists(filepath))
+    assert not files.file_exists(filepath)
 
 
 def test_cycle_read_write_text():
@@ -81,7 +81,7 @@ def test_cycle_read_write_text():
     filepath = "/tmp/tmpfile12395698744"
     if os.path.exists(filepath):
         os.remove(filepath)
-    assert not (files.file_exists(filepath))
+    assert not files.file_exists(filepath)
     data = "This is my text"
     files.write(filepath, data, istext=True)
     assert files.file_exists(filepath)
@@ -89,7 +89,7 @@ def test_cycle_read_write_text():
     data1 = files.read(filepath, istext=True)
     assert data == data1
     os.remove(filepath)
-    assert not (files.file_exists(filepath))
+    assert not files.file_exists(filepath)
 
 
 def test_cycle_read_write_binary():
@@ -97,7 +97,7 @@ def test_cycle_read_write_binary():
     filepath = "/tmp/tmpfile12395698744"
     if os.path.exists(filepath):
         os.remove(filepath)
-    assert not (files.file_exists(filepath))
+    assert not files.file_exists(filepath)
     data = b"This is my text"
     files.write(filepath, data)
     assert files.file_exists(filepath)
@@ -105,7 +105,7 @@ def test_cycle_read_write_binary():
     data1 = files.read(filepath)
     assert data == data1
     os.remove(filepath)
-    assert not (files.file_exists(filepath))
+    assert not files.file_exists(filepath)
 
 
 def test_cycle_chmod():
@@ -113,11 +113,11 @@ def test_cycle_chmod():
     filepath = "/tmp/tmpfile12395698744"
     if os.path.exists(filepath):
         os.remove(filepath)
-    assert not (files.file_exists(filepath))
+    assert not files.file_exists(filepath)
     Path(filepath).touch()
     assert files.file_exists(filepath)
     files.set_chmod(filepath, 0o700)
-    ch = files.get_chmod(filepath)
-    assert ch == 33216
+    chmode = files.get_chmod(filepath)
+    assert chmode == 33216
     os.remove(filepath)
-    assert not (files.file_exists(filepath))
+    assert not files.file_exists(filepath)
